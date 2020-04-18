83f7c53657166ac9732acd8d0ad452077adc5342
nghiatran1792000 <nghia.tran.179@hcmut.edu.vn>
2020-04-18 10:07:26 +0700

    main.c update os

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* factorial(const int aNumber) {
+    long fact = 1;
+    for (int i = 1; i <= aNumber; i++) fact *= i;
+    char* temp = malloc(aNumber * sizeof(char));
+    int index = 0;
+    while (fact >= 10) {
+        int digit = fact % 10;
+        *(temp + index) = digit + '0';
+        fact = fact / 10;
+        index++;
+    }
+    *(temp + index) = fact + '0';
+    for (int i = 0; i <= index / 2; i++) {
+        char tempChar = temp[i];
+        temp[i] = temp[index - i];
+        temp[index - i] = tempChar;
+    }
+    return temp;
+}
+
+int main() {
+    int a = 10;
+    char b = a + '0';
+    printf("%s", factorial(a));
+    return 0;
+}
